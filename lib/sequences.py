326da70908306384ced0b458d2a51b353d3cdc78
CahenTaylor <cahentaylor@gmail.com>
2023-05-03 10:53:05 -0600

    test passing

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        fib_list = []
+        print(fib_list)
+    elif length == 1:
+        fib_list = [0]
+        print(fib_list)
+    else:
+        fib_list = [0, 1]
+        for i in range(2, length):
+            last = fib_list[-1]
+            pent = fib_list[-2]
+            next = last + pent
+            fib_list.append(next)
+        print(fib_list)
